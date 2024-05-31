33dd4c2493bde60375ab9a390a835de770c23184
nimartin99 <nic0m4rtin@gmail.com>
2024-05-31 16:24:22 +0200

    assignment05 - exercise 1d)

--- assignment05/display.py
+++ assignment05/display.py
@@ -1,11 +1,25 @@
 # Script to display the points
 import open3d as o3d
 import numpy as np
+import csv
+import math
 
 
 if __name__ == "__main__":
+    points = [] # [x, y, z] coordinates in m
 
-    points = None # [x, y, z] coordinates in m
+    with open('LIDAR_points.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            coords = row[0].split(",")
+            distance = float(coords[0])
+            tilt = float(coords[1])
+            azimuth = float(coords[2])
+            
+            x = distance * math.cos(azimuth) * math.cos(tilt)
+            y = distance * math.sin(azimuth) * math.cos(tilt)
+            z = distance * math.cos(tilt)
+            points.append([x, y, z])
 
     pcd= o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
